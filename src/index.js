5e76037480dce44418afbcef7af94b1d77d78254
Tim Watson <tim.watson.nz@gmail.com>
2017-08-10 20:11:58 +0200

    Basic todo list implementation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,98 @@
 import React from "react";
 import { render } from "react-dom";
 
-class Button extends React.Component {
-    render() {
-        return <button>{this.props.text}</button>
+class TodoForm extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            todos: [],
+            idCount: 0
+        };
+
+        this.addItem = this.addItem.bind(this);
+    }
+
+    addItem(todoItemText){
+        
+        const todo = { id: this.state.idCount, text: todoItemText };
+        const updatedIds = this.state.idCount + 1;
+        this.state.todos.push(todo);
+
+        this.setState({todos: this.state.todos});
+        this.setState({idCount: updatedIds});
+
+        console.log(this.state.todos);
+    }
+
+    render (){
+        return (
+            <div>
+                <h1>I am a TodoList</h1>
+                <AddItem addItem={this.addItem}/>
+                <TodoList todos={this.state.todos} />
+            </div>
+        )
     }
 }
 
-window.onload = () => {
-    var root = document.createElement("div");
-    root.id = "react-root";
-    document.body.appendChild(root);
+class TodoList extends React.Component{
+    constructor(props){
+        super(props);
+    }
 
-    render(<Button text="Hello, World!" />, root);
-};
+    render(){
+        const nodes = this.props.todos.map((todo) => {
+            return (<TodoItem key={todo.id} text={todo.text}></TodoItem>   )
+        });
+        return (<ul>{nodes}</ul>);
+    }
+}
+
+class AddItem extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = { value: "" };
+
+        this.onKeyPress = this.onKeyPress.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    onKeyPress(event){
+        if(event.key === "Enter"){
+            this.props.addItem(this.state.value);
+            this.setState({value: ""});
+        }
+    }
+
+    handleChange(event){
+        this.setState({value: event.target.value });
+    }
+
+    render(){
+        return (
+            <input type="text" placeholder="Add a new item" 
+                   onKeyPress={this.onKeyPress} 
+                   value={this.state.value}
+                   onChange={this.handleChange}
+                   />
+        )
+    }
+}
+
+class TodoItem extends React.Component {
+    constructor(props){
+        super(props);
+
+        // State has completion
+        // and text?
+    }
+    render() {
+        return (
+            <li>{this.props.text}</li>
+        )
+    }
+}
+
+render(<TodoForm />, document.getElementById("app"));
